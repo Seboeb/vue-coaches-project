faec450cfb73832a93a94e498fd26e4d58aa98d9
Sebastiaan Hekner <sebastiaan.hekner@secrid.com>
2021-08-25 12:54:37 +0200

    Auth redirecting and router guards

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -10,7 +10,9 @@
       <base-card>
         <div class="controls">
           <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
-          <base-button link to="/auth" v-if="!isLoggedIn">Login</base-button>
+          <base-button link to="/auth?redirect=register" v-if="!isLoggedIn"
+            >Login to Register as Coach</base-button
+          >
           <base-button v-if="!isCoach && !isLoading && isLoggedIn" link to="/register"
             >Register a coach</base-button
           >
